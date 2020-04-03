647ed937a2497578b10cf68d2cf86034fa612496
drawiin <vini65599@gmail.com>
2020-04-03 10:18:09 -0400

    change the styles

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ for(let i = 1; i <= 20;i++){
     const element = document.createElement('img')
     element.src = `./assets/img/album/img${i}.jpg`
     imageContainer.appendChild(element)
+    setTimeout(1000)
 }
