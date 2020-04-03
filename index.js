9d8c207ff3d6ec3f6717b997409b1b767a0b30b5
drawiin <vini65599@gmail.com>
2020-04-03 00:54:10 -0400

    adding images

---
+++ index.js
@@ -0,0 +1,7 @@
+const imageContainer = document.getElementById('image-container')
+
+for(let i = 1; i <= 20;i++){
+    const element = document.createElement('img')
+    element.src = `./assets/img/album/img${i}.jpg`
+    imageContainer.appendChild(element)
+}
